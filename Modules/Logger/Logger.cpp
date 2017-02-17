54f2ca750ab28cfe2f2d736393a75139ae584166
Xorgon <norman.reggox@gmail.com>
2017-02-17 04:05:40 +0000

    Bug fixes for Logger module.

--- Modules/Logger/Logger.cpp
+++ Modules/Logger/Logger.cpp
@@ -6,7 +6,8 @@
 
 Logger::Logger(int sdChipSelect) {
     SD.begin(sdChipSelect);
-    logName = "test";
+    logName = getNextName();
+    Serial.println("Logging to: " + logName + ".QFL");
     logDir = "logs";
     log("System", "Startup.");
 }
@@ -16,7 +17,7 @@ Logger::Logger() {};
 void Logger::log(String tag, String data) {
     String logLine = "[" + parseMillis(millis()) + "][" + tag + "] " + data;
     Serial.println(logLine);
-    File logFile = SD.open(logDir + "/" + getNextName() + ".qfl", FILE_WRITE);
+    File logFile = SD.open("/" + logDir + "/" + logName + ".qfl", FILE_WRITE);
     logFile.println(logLine);
     logFile.close();
 }
@@ -48,18 +49,24 @@ String Logger::parseMillis(uint32_t millis) {
 }
 
 String Logger::getNextName() {
-    File dir = SD.open("/" + dir);
+    File dir = SD.open("/logs/");
     int maxFlightN = 0;
-    while (dir.available() > 0) {
+    while (true) {
         File file = dir.openNextFile();
+        if (! file) {
+            // No more files
+            break;
+        }
         String name = file.name();
         int len = name.length();
-        if (name.substring(len - 4, len) == "qfl") {
-            int flightN = name.substring(6, 9).toInt();
+        if (name.substring(len - 3, len) == "QFL") {
+            name.replace("FLIGHT", "");
+            name.replace(".QFL", "");
+            int flightN = name.toInt();
             if (flightN > maxFlightN) {
                 maxFlightN = flightN;
             }
         }
     }
-    return "flight" + String(maxFlightN + 1);
+    return "FLIGHT" + String(maxFlightN + 1);
 }
