b453b75b82f3db6c5828be06c23f0ac7d5eb47e1
Xorgon <norman.reggox@gmail.com>
2017-04-25 14:51:40 +0100

    Fixed GrabberTest sketch (was using Java instantiation syntax -_-).

--- Testing/GrabberTest/GrabberTest.ino
+++ Testing/GrabberTest/GrabberTest.ino
@@ -1,45 +1,17 @@
 #include <Arduino.h>
-//#include <SerialLogger.h>
-//#include <Grabber.h>
-#include "Servo.h"
+#include <SerialLogger.h>
+#include <Grabber.h>
 
+SerialLogger logger;
+Grabber grabber;
 
-//SerialLogger logger;
-//Grabber grabber;
-
-Servo servo;
-int pos;
 
 void setup() {
-//    logger = new SerialLogger();
-//    grabber = new Grabber(10, &logger);
-    servo = Servo();
-    servo.attach(10);
-    servo.write(0);
-    pos = 0;
+    logger = SerialLogger();
+    grabber = Grabber(10, &logger);
 }
 
 void loop() {
-
-    moveTo(90);
-    delay(1000);
-    moveTo(0);
+    grabber.release();
     while(1);
-}
-
-void moveTo(int loc) {
-    if (pos == loc) {
-        return;
-    }
-    if (pos < loc) {
-        for (pos; pos < loc; pos++) {
-            servo.write(pos);
-            delay(5);
-        }
-    } else {
-        for (pos; pos > loc; pos--) {
-            servo.write(pos);
-            delay(5);
-        }
-    }
 }
