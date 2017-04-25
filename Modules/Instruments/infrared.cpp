1e606642b02a1d29565d0c56b43c1e32d49234e1
Perryfung <hlf1n16@soton.ac.uk>
2017-04-25 02:02:46 +0100

    Update infrared.cpp

--- Modules/Instruments/infrared.cpp
+++ Modules/Instruments/infrared.cpp
@@ -22,22 +22,55 @@ void infrared::Calculate() {
 
 	int mean[3] = { 0 }; 
 	long sensorsum[3] = { 0 };
-	int sensorValue[3] = { 0 };
-	sensorValue[0] = analogRead(A0);   //Body Frame x direction infrared sensor
-	sensorValue[1] = analogRead(A1);   //....y.....
-	sensorValue[2] = analogRead(A2);   //....z.....
+	float sensorValue[3] = { 0 };
 
-	for (int n = 0; n < 3; n++) {
-		for (int i = 0; i < 3; i++) {
-			if (abs(sensorValue[n] - mean[n]) / mean[n] < 10) {
-				sensorsum[n] = sensorsum[n] + sensorValue[n];
-				mean[n] = sensorsum[n] / 3;
-			}
-		}
-	}
+  
+        do{
+            for (int n = 0; n < 3; n++){
+            sensorValue[0] = analogRead(A0);   //Body Frame x direction infrared sensor
+            sensorsum[0] = sensorsum[0] + sensorValue[0];
+            }
+            mean[0] = sensorsum[0]/3;
+            sensorsum[0] = 0;        //clear and run
+            //Serial.print("mean x:");  //debug
+            //Serial.println(mean[0]);
+            
+
+        }while(abs(sensorValue[0] - mean[0]) / mean[0] > 10);
+
+
+        do{
+            for (int n = 0; n < 3; n++){
+            sensorValue[1] = analogRead(A1);   //Body Frame y direction infrared sensor
+            sensorsum[1] = sensorsum[1] + sensorValue[1];
+            }
+            mean[1] = sensorsum[1]/3;
+            sensorsum[1] = 0;        //clear and run
+
+        }while(abs(sensorValue[1] - mean[1]) / mean[1] > 10);
+
+
+        do{
+            for (int n = 0; n < 3; n++){
+            sensorValue[2] = analogRead(A2);   //Body Frame z direction infrared sensor
+            sensorsum[2] = sensorsum[2] + sensorValue[2];
+            }
+            mean[2] = sensorsum[2]/3;
+            sensorsum[2] = 0;        //clear and run
+            
+
+        }while(abs(sensorValue[2] - mean[2]) / mean[2] > 10);
+
+
+
+
+
 
 	for (int j = 0; j < 3; j++) {															// To relate the sensor analogue value to distance
-		sensorValue[j] = 28000 * pow(mean[j], -1.58);										// another set of calculation could be given by 
+		sensorValue[j] = 28000 * pow(mean[j], -1.58);										// another set of calculation could be given by
+        //Serial.print(j);
+        //Serial.print("_Absolute Value:");  //debug
+        //Serial.println(sensorValue[j]);
 	}																						// Distance = 12348.85 * ADC^{-1.05} - 4
 																													
 
