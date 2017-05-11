a6a9930b7845028a2cb42db89a3ad1051f37e563
Xorgon <norman.reggox@gmail.com>
2017-05-11 21:21:42 +0100

    Improved ReceiverGearEmulator.

--- Testing/ReceiverGearEmulator/ReceiverGearEmulator.ino
+++ Testing/ReceiverGearEmulator/ReceiverGearEmulator.ino
@@ -2,10 +2,13 @@
 #include <Servo.h>
 
 Servo out;
+Servo out2;
 
 void setup() {
     out.attach(10);
     out.writeMicroseconds(1900);
+    out2.attach(11);
+    out2.writeMicroseconds(1200);
 }
 
 void loop() {
